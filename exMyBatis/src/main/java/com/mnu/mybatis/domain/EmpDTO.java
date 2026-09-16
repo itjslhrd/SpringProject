c3592b8703174de9c13aa15a904463ebf54276f1
jongmin na <webs1027@gmail.com>
2026-09-16 11:25:55 +0900

    9월16일-2

--- exMyBatis/src/main/java/com/mnu/mybatis/domain/EmpDTO.java
+++ exMyBatis/src/main/java/com/mnu/mybatis/domain/EmpDTO.java
@@ -3,6 +3,8 @@ package com.mnu.mybatis.domain;
 import lombok.Data;
 
 @Data
+//@NoArgsConstructor  // 기본 생성자 강제 추가 (Spring, MyBatis, JPA 등이 객체 생성할 때 필수)
+//@AllArgsConstructor // 모든 필드를 인자로 받는 생성자 추가
 public class EmpDTO {
 	private int eno;
 	private String ename;
--- exSample/src/main/java/com/mnu/sample/domain/BoardDTO.java
+++ exSample/src/main/java/com/mnu/sample/domain/BoardDTO.java
@@ -3,6 +3,8 @@ package com.mnu.sample.domain;
 import lombok.Data;
 
 @Data
+//@NoArgsConstructor  // 기본 생성자 강제 추가 (Spring, MyBatis, JPA 등이 객체 생성할 때 필수)
+//@AllArgsConstructor // 모든 필드를 인자로 받는 생성자 추가
 public class BoardDTO {
 	private int idx;			//고유번호(키)
 	private String name;		//작성자
